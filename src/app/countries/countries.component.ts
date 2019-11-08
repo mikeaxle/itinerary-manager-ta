da8fa27f528256814a5b204f7219825adceb8b5d
Saint Michael <mikeaxle2006@gmail.com>
2019-11-08 10:29:05 +0200

    started country editor

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,15 +1,112 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Country} from '../model/country';
+import {Region} from '../model/region';
+import {MatDialog, MatPaginator, MatTableDataSource} from '@angular/material';
+import {Router} from '@angular/router';
+import {DataService} from '../services/data.service';
+import {ConfirmComponent} from '../shared/confirm/confirm.component';
+import {EditorComponent} from '../shared/editor/editor.component';
+
+// country interface
+export interface Country {
+  name: string;
+  regions: Region
+  phoneNumbers: string[];
+  flag: string;
+}
 
 @Component({
   selector: 'app-countries',
-  templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.css']
+  styleUrls: ['./countries.component.css'],
+  templateUrl: './countries.component.html'
 })
-export class CountriesComponent implements OnInit {
+export class CountriesComponent implements OnInit, OnDestroy {
+  countries = []
+  error: any;
+  displayedColumns = [ 'Image', 'Title', 'Country', 'Region', 'Type', 'Actions'];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  // @ViewChild(MatSort, {static: true}) sort: MatSort;
+  private ref;
+  private countriesSubscription$;
 
-  constructor() { }
+  constructor(public router: Router, public data: DataService, public dialog: MatDialog) {}
 
   ngOnInit() {
+    // get countries
+    this.countriesSubscription$ = this.data.firestore.collection('countries')
+      .snapshotChanges()
+      .subscribe(snapshots => {
+        // init countries array
+        this.countries = [];
+
+        snapshots.forEach(snapshot => {
+          let item = {};
+          item = snapshot.payload.doc.data();
+          item[`key`] = snapshot.payload.doc.id;
+          this.countries.push(item);
+        });
+
+        // init data source
+        this.dataSource = new MatTableDataSource(this.countries);
+
+        // init data source
+        this.dataSource.paginator = this.paginator;
+
+        // init sort
+        // this.dataSource.sort = this.sort;
+      });
   }
 
+  // function to open confirm delete dialog
+  openDelete(item) {
+
+    const dialogRef = this.dialog.open(ConfirmComponent, {
+      width: '480px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result !== undefined) {
+        // if result is true
+        if (result) {
+          this.deleteCountry(item);
+        }
+      }
+    });
+  }
+
+  // function to delete item
+  deleteCountry(item) {
+    this.data.deleteObjectFromFirebase(`countries/${item.key}`, 'country');
+  }
+
+  // function to add new inventory item
+  addNew() {
+    this.dialog.open(EditorComponent, {
+      data: {
+        item: null,
+        new: true,
+        type: 'country'
+      }
+    });
+  }
+
+// function to edit inventory item
+  editCountry(country) {
+    // todo: fix region control when editing inventory item
+    this.dialog.open(EditorComponent, {
+      autoFocus: false,
+      data: {
+        item: country,
+        new: false,
+        type: 'inventory'
+      },
+      maxHeight: '80vh',
+      maxWidth: '60vw'
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.countriesSubscription$.unsubscribe();
+  }
 }
--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {DataService} from '../services/data.service';
+import {DataService} from '../../services/data.service';
 import Swal from 'sweetalert2';
 
 @Component({
