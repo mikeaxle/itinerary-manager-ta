1c1f8de053786dcd0b8403517bf70f5c9a145fdd
Saint Michael <mikeaxle2006@gmail.com>
2020-02-13 14:14:07 +0200

    fix: force heroku ssl
    added force ssl plugin to server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var forceSsl = require('force-ssl-heroku');
 
 const app = express();
+app.use(forceSsl);
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
